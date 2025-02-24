f153c5e60420883e1877d8d97bd9799da8328df6
stevezemlicka <stevezemlicka@gmail.com>
2025-02-24 16:12:46 -0600

    corrected nfs mount config

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -21,11 +21,11 @@
   };
   fileSystems."/mnt/general" = {
     device = "172.23.1.3:/mnt/lh01/general";
-    options = [ "rw", "timeo=14", "intr", "async" ];
+    options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "async" ];
   };
   fileSystems."/mnt/media" = {
     device = "172.23.1.3:/mnt/lh01/media";
-    options = [ "rw", "timeo=14", "intr", "asysn" ];
+    options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "asysn" ];
   };
   environment.systemPackages = with pkgs; [ networkmanagerapplet ];
 }
