32437534e43efd76705637e7950bca645902eb59
stevezemlicka <stevezemlicka@gmail.com>
2025-02-24 16:34:50 -0600

    nfs changes

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -23,9 +23,9 @@
     device = "172.23.1.3:/mnt/lh01/general";
     options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "async" ];
   };
-  #fileSystems."/mnt/media" = {
-  #  device = "172.23.1.3:/mnt/lh01/media";
-  #  options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "async" ];
-  #};
+  fileSystems."/mnt/media" = {
+    device = "172.23.1.3:/mnt/lh01/media";
+    options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "async" ];
+  };
   environment.systemPackages = with pkgs; [ networkmanagerapplet ];
 }
