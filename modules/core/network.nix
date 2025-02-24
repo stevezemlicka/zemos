5dd9dee113752508d43bf74ad3d13b4c82c90fb7
stevezemlicka <stevezemlicka@gmail.com>
2025-02-24 16:07:02 -0600

    add NFS mounts to network.nix

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -19,6 +19,13 @@
       ];
     };
   };
-
+  fileSystems."/mnt/general" = {
+    device = "172.23.1.3:/mnt/lh01/general";
+    options = [ "rw", "timeo=14", "intr", "async" ];
+  };
+  fileSystems."/mnt/media" = {
+    device = "172.23.1.3:/mnt/lh01/media";
+    options = [ "rw", "timeo=14", "intr", "asysn" ];
+  };
   environment.systemPackages = with pkgs; [ networkmanagerapplet ];
 }
