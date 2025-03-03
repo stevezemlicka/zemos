b20928c1d14b91e8fc3f251a4bfcb4bd14bc66b6
stevezemlicka <stevezemlicka@gmail.com>
2025-03-03 13:01:55 -0600

    modify nfs-mount to fix subsequent gen issue with lib.mkForce

--- modules/core/nfs-mounts.nix
+++ modules/core/nfs-mounts.nix
@@ -1,11 +1,11 @@
 { pkgs, host, options, ... }:
 {
   fileSystems."/mnt/general" = {
-    device = "172.23.1.3:/mnt/lh01/general";
+    device = lib.mkForce "172.23.1.3:/mnt/lh01/general";
     options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "async" ];
   };
   fileSystems."/mnt/media" = {
-    device = "172.23.1.3:/mnt/lh01/media";
+    device = lib.mkForce "172.23.1.3:/mnt/lh01/media";
     options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "async" ];
   };
 }
