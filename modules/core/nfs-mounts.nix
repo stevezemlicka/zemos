750562ff8c4348125ca0c36ce6f8ce86c6dc58c2
stevezemlicka <stevezemlicka@gmail.com>
2025-03-03 15:05:34 -0600

    switch nfs to libDefault rather than libForce

--- modules/core/nfs-mounts.nix
+++ modules/core/nfs-mounts.nix
@@ -1,11 +1,11 @@
 { pkgs, lib, host, options, ... }:
 {
   fileSystems."/mnt/general" = {
-    device = lib.mkForce "172.23.1.3:/mnt/lh01/general";
+    device = lib.mkDefault "172.23.1.3:/mnt/lh01/general";
     options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "async" ];
   };
   fileSystems."/mnt/media" = {
-    device = lib.mkForce "172.23.1.3:/mnt/lh01/media";
+    device = lib.mkDefault "172.23.1.3:/mnt/lh01/media";
     options = [ "x-systemd.automount" "rw" "timeo=14" "intr" "async" ];
   };
 }
